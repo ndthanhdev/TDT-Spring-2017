62d9d0abd1d7323bd09982ec99b297470b391d06
thanhtdt <thanhtdt@outlook.com>
2017-03-03 08:50:55 +0700

    ...

--- TDT IT Forum/Frontend/src/app/register/register.component.ts
+++ TDT IT Forum/Frontend/src/app/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RegisterInformation} from '../models/registerInformation';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  admissionYears: number[] = [];
+  model: RegisterInformation;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    let currentYear = (new Date()).getFullYear();
+    for (let _i = currentYear; _i >= 1997; _i--) {
+      this.admissionYears.push(_i);
+    }
+    this.model = new RegisterInformation('', '', '', currentYear, '', '');
+  }
+
+  submit():void {
+    console.log(this.model.admissionYear);
   }
 
 }
