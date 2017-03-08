6dd3016eb94cac42238059b81c0f42ab3998b47e
Duy Thanh Nguyen <thanhtdt@outlook.com>
2017-03-08 15:11:56 +0700

    debug

--- TDT IT Forum/Frontend/src/app/register/register.component.ts
+++ TDT IT Forum/Frontend/src/app/register/register.component.ts
@@ -1,32 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {RegisterInformation} from '../models/registerInformation';
-import {RegisterService} from "./register.service";
+﻿import { Component, OnInit } from '@angular/core';
+import { RegisterInformation } from '../models/registerInformation';
+import { RegisterService } from "./register.service";
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    moduleId: module.id,
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  admissionYears: number[] = [];
-  model: RegisterInformation;
+    admissionYears: number[] = [];
+    model: RegisterInformation;
 
-  constructor(private service: RegisterService) {
-  }
-
-  ngOnInit() {
-    let currentYear = (new Date()).getFullYear();
-    for (let _i = currentYear; _i >= 1997; _i--) {
-      this.admissionYears.push(_i);
+    constructor(private service: RegisterService) {
     }
-    this.model = new RegisterInformation('', '', '', currentYear, '', '');
-  }
 
-  onSubmit(): void {
-    this.service.login('admin', 'admin');
-  }
+    ngOnInit() {
+        let currentYear = (new Date()).getFullYear();
+        for (let _i = currentYear; _i >= 1997; _i--) {
+            this.admissionYears.push(_i);
+        }
+        this.model = new RegisterInformation('', '', '', currentYear, '', '');
+    }
+
+    onSubmit(): void {
+        this.service.login('admin', 'admin');
+    }
 
 
 }
--- TDT IT Forum/Frontend/src/app/register/register.service.ts
+++ TDT IT Forum/Frontend/src/app/register/register.service.ts
@@ -1,7 +1,7 @@
-/**
+﻿/**
  * Created by thanh on 3/7/2017.
  */
-import { Http, Response, Headers, RequestOptions } from '@angular/http'
+import { Http, Response, Headers, RequestOptions} from '@angular/http'
 import { Injectable } from '@angular/core'
 
 import { Observable } from 'rxjs/Observable';
@@ -10,38 +10,39 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class RegisterService {
-  constructor(private http: Http) {
-  }
-
-  login(username: String, password: String): void {
-    let headers = new Headers({});
-    // headers.append('Content-Type', 'application/json');
-    // headers.append('Access-Control-Allow-Headers', 'Content-Type');
-    // headers.append('Content-Type', 'GET');
-    // headers.append('Access-Control-Allow-Methods', '*');
-    let options = new RequestOptions({ headers: headers });
-    this.http.get('http://localhost:62636/test').subscribe(value => console.log(value.json()));
-
-  }
-
-  private extractData(res: Response) {
-    console.log(res);
-    let body = res.json();
-    return body.data || {};
-  }
-
-  private handleError(error: Response | any) {
-    // In a real world app, we might use a remote logging infrastructure
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
+    constructor(private http: Http) {
     }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
-  }
+
+    login(username: String, password: String): void {
+        let headers = new Headers({});
+        // headers.append('Content-Type', 'application/json');
+        // headers.append('Access-Control-Allow-Headers', 'Content-Type');
+        // headers.append('Content-Type', 'GET');
+        // headers.append('Access-Control-Allow-Methods', '*');
+        let options = new RequestOptions({ headers: headers });
+        this.http.get('http://localhost:62636/test').subscribe(value => console.log(value.json()));
+
+
+    }
+
+    private extractData(res: Response) {
+        console.log(res);
+        let body = res.json();
+        return body.data || {};
+    }
+
+    private handleError(error: Response | any) {
+        // In a real world app, we might use a remote logging infrastructure
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
 
 }
