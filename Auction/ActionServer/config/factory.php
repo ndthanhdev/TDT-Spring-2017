efe24be0da09df7afa7fe01e9b233717b35e1a19
DuyThanhNguyen <thanhtdt@outlook.com>
2017-05-03 01:36:59 +0700

    ...

--- Auction/ActionServer/config/factory.php
+++ Auction/ActionServer/config/factory.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'USER_AMOUNT' => 10,
-    'ITEM_AMOUNT' => 20,
+    'ITEM_AMOUNT' => 30,
     'BID_AMOUNT' => 100,
 ];
--- Auction/ActionServer/database/factories/ItemFactory.php
+++ Auction/ActionServer/database/factories/ItemFactory.php
@@ -2,9 +2,9 @@
 
 $factory->define(\App\Entities\Item::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name(),
+        'name' => $faker->sentence,
         'description' => $faker->text(),
-        'image' => $faker->name(),
+        'image' => $faker->imageUrl(640, 800, 'cats'),
         'ending_time' => $faker->dateTimeThisMonth($max = 'now + 3days'),
         'starting_price' => $faker->numberBetween(1, 1000) * 1000,
         'user_id' => $faker->numberBetween(1, config('factory.USER_AMOUNT'))
